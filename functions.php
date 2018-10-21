3796173241bdeb1a9159d47ba182a7ee74463234
Robin <robin.sparky@gmail.com>
2018-10-21 19:17:37 -0400

    modified dequing of appointment-pro default.css

--- functions.php
+++ functions.php
@@ -24,12 +24,18 @@ function care_mci_theme_css() {
 	wp_enqueue_style( 'default-css', get_stylesheet_directory_uri()."/css/default.css" );
 	wp_enqueue_style( 'element-style', get_template_directory_uri() . '/css/element.css' );
 	wp_enqueue_style( 'media-responsive', get_template_directory_uri(). '/css/media-responsive.css');
-	wp_dequeue_style( 'appointment-default',get_template_directory_uri() .'/css/default.css');
+	//wp_dequeue_style( 'appointment-default',get_template_directory_uri() .'/css/default.css');
 
 	//javascript
 	wp_enqueue_script('care-common-js', get_stylesheet_directory_uri()."/js/care-message-window.js");
 }
 add_action( 'wp_enqueue_scripts', 'care_mci_theme_css',999);
+
+function change_default_css( ) {
+    wp_dequeue_style( 'appointment-default');
+    wp_deregister_style( 'appointment-default');   
+}
+add_filter( 'wp_enqueue_styles', 'change_default_css', PHP_INT_MAX );
 
 /*
 * MCI Setup
