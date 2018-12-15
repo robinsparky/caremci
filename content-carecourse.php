153eff50e0977c504fd9cd04b6afafbc31984cf2
robinsparky <robin.sparky@gmail.com>
2018-12-15 14:57:37 -0500

    Moved sessions to bottom

--- content-carecourse.php
+++ content-carecourse.php
@@ -16,73 +16,72 @@
 					&nbsp;<span>Duration: <?php echo $duration ?> hours</span>
 					&nbsp;<span><?php echo $instructions ?></span>
 				</h4>
+				<?php 
+				// call editor content of post/page	s
+				the_content( __('Read More', 'appointment' ) );
+				wp_link_pages( );
+				?>
 				<?php $courseId = get_the_ID(); 
 				    $allSessions = CourseSession::getCourseSessions( $courseId );
 					error_log(__FILE__ . " courseId=$courseId");
 					error_log( print_r( $allSessions, true ) );
 				?>
-				<?php if( count( $allSessions ) > 0 ) { ?>
-					<table class="course_sessions"> 
-						<caption>Course Sessions</caption>
-						<thead>
+				<table class="course_sessions"> 
+					<caption><?php echo __("Upcoming Sessions", CARE_TEXTDOMAIN ) ?></caption>
+					<thead>
+					<tr>
+						<th>Location</th>
+						<th>Start Date</th>
+						<th>Start Time</th>
+						<th>End Date</th>
+						<th>End Time</th>
+						<th>Registration</th>
+					</tr>
+					</thead>
+					<tbody>
+					<?php foreach( $allSessions as $session ) { 
+						$now = new DateTime();
+						error_log($session['event_start_date']);
+						$start = new DateTime( $session['event_start_date'] );
+						if ( $start < $now ) continue;
+						?>
 						<tr>
-							<th>Location</th>
-							<th>Start Date</th>
-							<th>Start Time</th>
-							<th>End Date</th>
-							<th>End Time</th>
-							<th>Registration</th>
+							<td><?php echo $session['location_name']?></td>
+							<td><?php echo $session['event_start_date']?></td>
+							<td><?php echo $session['event_start_time']?></td>
+							<td><?php echo $session['event_end_date']?></td>
+							<td><?php echo $session['event_end_time']?></td>
+							<td>
+							<?php
+								$user = wp_get_current_user();
+								if ( is_user_logged_in() 
+										&& ( in_array('um_caremember', $user->roles) || in_array('administrator', $user->roles))
+									) { 
+								?>
+								<button id="carecourse_register" type="button" 
+									data-courseId="<?php echo get_the_ID(); ?>" 
+									data-courseName="<?php echo get_the_title()?>" 
+									data-startDate="<?php echo $session['event_start_date'] ?>"
+									data-startTime="<?php echo $session['event_start_time']?>"
+									data-endDate="<?php echo $session['event_end_date'] ?>"
+									data-endTime="<?php echo $session['event_end_time']?>"
+									data-sessionLocation = "<?php echo $session['location_name']?>"
+									data-sessionSlug="<?php echo site_url('events') . '/'. $session['event_slug']?>">
+									<?php echo __("Register", CARE_TEXTDOMAIN ) ?>
+								</button>
+								<?php }	else {
+									echo __("CARE members only", CARE_TEXTDOMAIN )?>
+								<?php } ?>
+								<?php 
+								if($needsApproval === 'yes') {
+									echo '<input id="passwordrequired" type="hidden" name="coursepassword" value="passwordrequired" />';
+								}
+								?>
+							</td>
 						</tr>
-						</thead>
-						<tbody>
-						<?php foreach( $allSessions as $session ) { 
-							$now = new DateTime();
-							error_log($session['event_start_date']);
-							$start = new DateTime( $session['event_start_date'] );
-							if ( $start < $now ) continue;
-							?>
-							<tr>
-								<td><?php echo $session['location_name']?></td>
-								<td><?php echo $session['event_start_date']?></td>
-								<td><?php echo $session['event_start_time']?></td>
-								<td><?php echo $session['event_end_date']?></td>
-								<td><?php echo $session['event_end_time']?></td>
-								<td>
-								<?php
-									$user = wp_get_current_user();
-									if ( is_user_logged_in() 
-									     && ( in_array('um_caremember', $user->roles) || in_array('administrator', $user->roles))
-										) { 
-								    ?>
-									<button id="carecourse_register" type="button" 
-										data-courseId="<?php echo get_the_ID(); ?>" 
-										data-courseName="<?php echo get_the_title()?>" 
-										data-startDate="<?php echo $session['event_start_date'] ?>"
-										data-startTime="<?php echo $session['event_start_time']?>"
-										data-endDate="<?php echo $session['event_end_date'] ?>"
-										data-endTime="<?php echo $session['event_end_time']?>"
-										data-sessionLocation = "<?php echo $session['location_name']?>"
-										data-sessionSlug="<?php echo site_url('events') . '/'. $session['event_slug']?>">
-										<?php echo __("Register", CARE_TEXTDOMAIN ) ?>
-									</button>
-									<?php }	else {
-										echo __("CARE members only", CARE_TEXTDOMAIN )?>
-									<?php } ?>
-									<?php 
-									if($needsApproval === 'yes') {
-										echo '<input id="passwordrequired" type="hidden" name="coursepassword" value="passwordrequired" />';
-									}
-									?>
-								</td>
-							</tr>
-					<?php } ?>
-						</tbody>
-					</table>
-				<?php } 	
-				// call editor content of post/page	s
-				the_content( __('Read More', 'appointment' ) );
-				wp_link_pages( );
-			   ?>
+				<?php } ?>
+					</tbody>
+				</table>
 		</div>
 	 </div>
 </div>
