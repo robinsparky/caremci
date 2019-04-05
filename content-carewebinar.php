80ec12485f5d75fb080a6f2b3c917ae983e2676f
robinsparky <robin.sparky@gmail.com>
2019-04-05 09:03:42 -0400

    Removed hard-coded domain name in link

--- content-carewebinar.php
+++ content-carewebinar.php
@@ -25,7 +25,7 @@
 				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
 				
 				<video id="<?php the_ID(); ?>" data-name="<?php the_Title()?>" width="600"  
-					poster="http://devel.care4nurses.org/wp-content/uploads/cropped-CARE_WordPress_Icon_512x512.png">
+					poster="<?php get_home_url(); ?>/wp-content/uploads/cropped-CARE_WordPress_Icon_512x512.png">
 					<source src="<?php echo $videoUrl ?>" type="video/mp4">
 					Your browser does not support HTML5 video.
 				</video>
