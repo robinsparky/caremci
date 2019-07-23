ca7b69bd3b37a222a03a739e2f0bd48a6fc59e0a
robinsparky <robin.sparky@gmail.com>
2019-07-23 14:00:03 -0400

    Removed unnecessary code

--- js/care-application-form.js
+++ js/care-application-form.js
@@ -1,100 +1,11 @@
 (function($) {
-
     $(document).ready(function() {       
-        let sig = '#care-reportmessage';
         console.log("Care Application Form");
-        //console.log(care_app_form);
-
-        var longtimeout = 60000;
-        var shorttimeout = 15000;
-        var selectionText;
-        var selectionId;
-        var startDate;
-        var endDate;
-
-        let ajaxFun = function( ) {
-            console.log('Care Application: ajaxFun');
-            let reqData =  { 'action': care_app_form.action      
-                            , 'security': care_app_form.security
-                            , 'user_id' : care_app_form.user_id
-                             };
-            //console.log( reqData );
-            console.log("************************Parameters:");
-            console.log( reqData );
-
-                // Send Ajax request with data 
-            let jqxhr = $.ajax( { url: care_pass_mgmt.ajaxurl    
-                                , method: "POST"
-                                , async: true
-                                , data: reqData
-                                , dataType: 'json'
-                        ,beforeSend: function( jqxhr, settings ) {
-                            //Disable the 'Done' button
-                            $(sig).html('Loading...');
-                        }})
-                    .done( function( res, jqxhr ) {
-                        console.log("done.res:");
-                        console.log(res);
-                        if( res.success ) {
-                            console.log('Success (res.data):');
-                            console.log(res.data);
-                            //Do stuff with data...
-                            $(sig).html( res.data.message );
-                            report = generateTable(res.data.returnData,care_pass_mgmt.titles,selectionText,'management-report')
-                            $('#' + care_pass_mgmt.reporttarget).append(report);
-                        }
-                        else {
-                            console.log('Done but failed (res.data):');
-                            console.log(res.data);
-                            var entiremess = res.data.message + " ...<br/>";
-                            for(var i=0; i < res.data.exception.errors.length; i++) {
-                                entiremess += res.data.exception.errors[i][0] + '<br/>';
-                            }
-                            $(sig).addClass('care-error');
-                            $(sig).html(entiremess);
-                        }
-                    })
-                    .fail( function( jqXHR, textStatus, errorThrown ) {
-                        console.log("fail");
-                        console.log("Error: %s -->%s", textStatus, errorThrown );
-                        var errmess = "Error: status='" + textStatus + "--->" + errorThrown;
-                        errmess += jqXHR.responseText;
-                        console.log('jqXHR:');
-                        console.log(jqXHR);
-                        $(sig).addClass('care-error');
-                        $(sig).html(errmess);
-                    })
-                    .always( function() {
-                        console.log( "always" );
-                        setTimeout(function(){
-                                    $(sig).html('');
-                                    $(sig).removeClass('care-error');
-                                }, shorttimeout);
-                    });
-            
-            return false;
-        }
-        
-        function formatDate( date ) {
-            var d = new Date(date),
-                month = '' + (d.getMonth() + 1),
-                day = '' + d.getDate(),
-                year = d.getFullYear();
-        
-            if (month.length < 2) month = '0' + month;
-            if (day.length < 2) day = '0' + day;
-        
-            return [year, month, day].join('-');
-        }
 
         //Moving out of Ontario?
         $('#move_where').hide();
        $('input[name="moving_out_of_ontario"]').change(function(e) {
-            console.log("moving_out_of_ontario change fired!");
-            // selectionText = e.target.options[e.target.selectedIndex].text;
-            // selectionId = e.target.value; 
-            // console.log("id=%s; title='%s'",selectionId, selectionText);
-            console.log( "Value is '%s'", e.target.value );
+            //console.log("moving_out_of_ontario change fired!");
             if( 'Yes' === e.target.value ) {
                 $('#move_where').show();
             }
@@ -103,32 +14,23 @@
             }
        });
        
-       //NNAS Application Submitted
-        $('#yes_nnas_report').hide();
-        $('#no_nnas_report').hide();
-       $('input[name="nnas-submitted"]').change(function(e) {
-            console.log("nnas-submitted change fired!");
-            // selectionText = e.target.options[e.target.selectedIndex].text;
-            // selectionId = e.target.value; 
-            // console.log("id=%s; title='%s'",selectionId, selectionText);
-            console.log( "Value is '%s'", e.target.value );
+        //NNAS Application Submitted
+        $('#yes-submitted-nnas').hide();
+        $('input[name="nnas-submitted"]').change(function(e) {
+            //console.log("nnas-submitted change fired!");
             if( 'Yes' === e.target.value ) {
-                $('#yes_nnas_report').show();
-                $('#no_nnas_report').hide();
+                $('#yes-submitted-nnas').show();
             }
             else {                
-                $('#yes_nnas_report').hide();
-                $('#no_nnas_report').show();
+                $('#yes-submitted-nnas').hide();
             }
         });
         
         //Received NNAS Assessment
+        $('#yes_nnas_report').hide();
+        $('#no_nnas_report').hide();
         $('input[name="nnas-report-received"]').change(function(e) {
-            console.log("nnas-report-received change fired!");
-            // selectionText = e.target.options[e.target.selectedIndex].text;
-            // selectionId = e.target.value; 
-            // console.log("id=%s; title='%s'",selectionId, selectionText);
-            console.log( "Value is '%s'", e.target.value );
+            //console.log("nnas-report-received change fired!");
             if( 'Yes' === e.target.value ) {
                 $('#yes_nnas_report').show();
                 $('#no_nnas_report').hide();
@@ -142,11 +44,7 @@
         //Have CNO Assessment Letter(s)
         $('#yes-cno-assessment').hide();
         $('input[name="have-cno-assessment"]').change(function(e) {
-            console.log("have-cno-assessment change fired!");
-            // selectionText = e.target.options[e.target.selectedIndex].text;
-            // selectionId = e.target.value; 
-            // console.log("id=%s; title='%s'",selectionId, selectionText);
-            console.log( "Value is '%s'", e.target.value );
+            //console.log("have-cno-assessment change fired!");
             if( 'Yes' === e.target.value ) {
                 $('#yes-cno-assessment').show();
             }
@@ -158,11 +56,7 @@
         //Courses taken in Canada
         $('.canadian-courses-taken').hide();
         $('input[name="taken-courses-canada"]').change(function(e) {
-            console.log("taken-courses-canada change fired!");
-            // selectionText = e.target.options[e.target.selectedIndex].text;
-            // selectionId = e.target.value; 
-            // console.log("id=%s; title='%s'",selectionId, selectionText);
-            console.log( "Value is '%s'", e.target.value );
+            //console.log("taken-courses-canada change fired!");
             if( 'Yes' === e.target.value ) {
                 $('.canadian-courses-taken').show();
             }
@@ -174,11 +68,7 @@
         //IELTS
         $('#ielts').hide();
         $('input[name="ielts-taken"]').change(function(e) {
-            console.log("ielts-taken change fired!");
-            // selectionText = e.target.options[e.target.selectedIndex].text;
-            // selectionId = e.target.value; 
-            // console.log("id=%s; title='%s'",selectionId, selectionText);
-            console.log( "Value is '%s'", e.target.value );
+            //console.log("ielts-taken change fired!");
             if( 'Yes' === e.target.value ) {
                 $('#ielts').show();
             }
@@ -190,11 +80,7 @@
         //CELBAN
         $('#celban').hide();
         $('input[name="celban-taken"]').change(function(e) {
-            console.log("celban-taken change fired!");
-            // selectionText = e.target.options[e.target.selectedIndex].text;
-            // selectionId = e.target.value; 
-            // console.log("id=%s; title='%s'",selectionId, selectionText);
-            console.log( "Value is '%s'", e.target.value );
+            //console.log("celban-taken change fired!");
             if( 'Yes' === e.target.value ) {
                 $('#celban').show();
             }
@@ -206,11 +92,8 @@
         //CLBA
         $('#clba').hide();
         $('input[name="clba-taken"]').change(function(e) {
-            console.log("clba-taken change fired!");
-            // selectionText = e.target.options[e.target.selectedIndex].text;
-            // selectionId = e.target.value; 
-            // console.log("id=%s; title='%s'",selectionId, selectionText);
-            console.log( "Value is '%s'", e.target.value );
+            // console.log("clba-taken change fired!");
+            // console.log( "Value is '%s'", e.target.value );
             if( 'Yes' === e.target.value ) {
                 $('#clba').show();
             }
